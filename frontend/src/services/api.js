e46b75dce6aea50fddca48ce38fa139818748802
srieevardhan <srieemadeshwaran@gmail.com>
2026-01-03 16:32:03 +0530

    added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-o8nm.onrender.com/api",
+  baseURL: "https://task-manager-o8nm.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
