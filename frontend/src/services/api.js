2ce63393e05e23a7455e3c2c0d83ab06f81d53b3
srieevardhan <srieemadeshwaran@gmail.com>
2026-01-03 21:19:01 +0530

    commited

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://task-uqdf.onrender.com",
+  baseURL: " https://task-uqdf.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
